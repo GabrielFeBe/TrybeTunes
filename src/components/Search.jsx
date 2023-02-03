fd6c6acf263e95874bbe1f939339c805ef69b179
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-02-03 18:29:06 -0300

    requesito 5

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,9 +2,37 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 export default class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      bandOrArtist: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+      // Após alterarmos o estado, chamamos a função que
+      // verificará os erros.
+    }, this.handleError);
+  };
+
   render() {
+    const { bandOrArtist } = this.state;
     return (
-      <div data-testid="page-search"><Header /></div>
+      <div data-testid="page-search">
+        <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          name="bandOrArtist"
+          onChange={ this.handleChange }
+          value={ bandOrArtist }
+        />
+        <button disabled={ bandOrArtist.length < 2 } data-testid="search-artist-button">Pesquisar</button>
+      </div>
     );
   }
 }
