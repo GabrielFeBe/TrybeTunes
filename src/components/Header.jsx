c5e87f136e5355fc0a9b74bfe8f493c0006fb9d6
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-02-03 18:20:23 -0300

    requesito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 export default class Header extends Component {
@@ -24,7 +25,12 @@ export default class Header extends Component {
     const { loading, name } = this.state;
     return (
       <header data-testid="header-component">
-        <div data-testid="header-user-name">{loading ? 'Carregando...' : `${name.name}`}</div>
+        <h1 data-testid="header-user-name">
+          {loading ? 'Carregando...' : `${name.name}`}
+        </h1>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
