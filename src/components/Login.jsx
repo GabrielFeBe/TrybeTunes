b695e5c5d83a25a31c4dcbd2af6a13f82b24f70a
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-02-03 17:44:04 -0300

    requesito 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+
+const TRES = 3;
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      login: '',
+      apiResponse: false,
+      trigger: false,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    const value = (target.type === 'checkbox') ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+      // Após alterarmos o estado, chamamos a função que
+      // verificará os erros.
+    }, this.handleError);
+  };
+
   render() {
+    const { history } = this.props;
+    const { login, trigger, apiResponse } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <input type="text" data-testid="login-name-input" onChange={ this.handleChange } name="login" />
+        <button
+          data-testid="login-submit-button"
+          onClick={ async () => {
+            this.setState({ trigger: true });
+            await createUser({ name: login });
+            history.push('/search');
+            this.setState({ trigger: false });
+          } }
+          disabled={ login.length < TRES }
+        >
+          Entrar
+
+        </button>
+        {console.log(this.props)}
+        {trigger && <p>Carregando...</p>}
+      </div>
     );
   }
 }
